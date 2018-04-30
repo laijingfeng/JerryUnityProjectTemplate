8083e0f59e56d84a96da8946fdcafcab87b7da7c
laijingfeng <337292616@qq.com>
2018-05-01 00:36:58 +0800

    update

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,4 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 
 public class Test : MonoBehaviour
@@ -6,16 +10,30 @@ public class Test : MonoBehaviour
     void Start()
     {
         Log("hahaha");
+        foreach (Type t in by_property)
+        {
+            UnityEngine.Debug.LogWarning(" " + t);
+        }
     }
+    
 
     void Update()
     {
-
     }
 
     [Conditional("DEBUG_VER")]
     static public void Log(object msg)
     {
         UnityEngine.Debug.LogError(msg);
+    }
+
+    public static List<Type> by_property
+    {
+        get
+        {
+            return (from type in Assembly.Load("Assembly-CSharp").GetTypes()
+                    where !type.ToString().Contains("XLua.")
+                    select type).ToList();
+        }
     }
 }
---
+++ Assets/XLuaUtil/XLuaMgr.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class XLuaMgr : MonoBehaviour
+{
+    static private XLuaMgr m_Inst;
+    static public XLuaMgr Inst
+    {
+        get
+        {
+            return m_Inst;
+        }
+    }
+
+    private LuaEnv luaEnv = null;
+
+    void Awake()
+    {
+        m_Inst = this;
+
+        luaEnv = new LuaEnv();
+        if (luaEnv != null)
+        {
+            luaEnv.AddLoader(CustomLoader);
+            LoadMain();
+        }
+    }
+
+    void LoadMain()
+    {
+        try
+        {
+            if (luaEnv != null)
+            {
+                luaEnv.DoString("require('Main')");
+            }
+        }
+        catch (System.Exception ex)
+        {
+            string msg = string.Format("xLua exception : {0}\n {1}", ex.Message, ex.StackTrace);
+            Debug.LogError(msg, null);
+        }
+    }
+
+    public static byte[] CustomLoader(ref string filepath)
+    {
+        Debug.Log("Load xLua script : " + filepath);
+        // TODO：此处从项目资源管理器加载lua脚本
+        TextAsset textAsset = (TextAsset)Resources.Load("xlua/" + filepath.Replace(".", "/") + ".lua");
+        //TextAsset textAsset = (TextAsset)ResourceMgr.instance.SyncLoad(ResourceMgr.RESTYPE.XLUA_SCRIPT, filepath).resObject;
+        if (textAsset != null)
+        {
+            return textAsset.bytes;
+        }
+        return null;
+    }
+
+    void Update()
+    {
+        if (luaEnv != null)
+        {
+            luaEnv.Tick();
+
+            if (Time.frameCount % 100 == 0)
+            {
+                luaEnv.FullGc();
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (luaEnv != null)
+        {
+            luaEnv.Dispose();
+            luaEnv = null;
+        }
+    }
+}
