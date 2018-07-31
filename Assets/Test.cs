59ee4564dcaada273fab137a2e79c0ee850c358a
laijingfeng <337292616@qq.com>
2018-07-31 14:10:30 +0800

    测试

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,15 +1,24 @@
 ﻿using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
+    public Text txt;
+
     void Start()
     {
         Haha();
+        txt.text = getCoin().ToString();
     }
 
     void Update()
     {
+    }
+
+    private int getCoin()
+    {
+        return 200;
     }
 
     public void Haha()
